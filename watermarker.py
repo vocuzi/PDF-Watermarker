769bcde14bbb6c063426630c21a82fe720c143df
Vipin Joshi <vipinjoshi@protonmail.com>
2019-05-04 13:00:19 +0530

    Fixed res issues with watermark

--- watermarker.py
+++ watermarker.py
@@ -2,7 +2,6 @@ from os import walk
 from reportlab.pdfgen import canvas
 from PyPDF2 import PdfFileWriter, PdfFileReader
 
-
 def get_pdf_list():
     r = []
     for (dirpath, dirnames, filenames) in walk("./input"):
@@ -10,9 +9,7 @@ def get_pdf_list():
         break
     return r
 
-
 canvas_width = 566
-
 # Create the watermark from an image
 c = canvas.Canvas('bin/sample-watermark.pdf')
 c.drawImage('bin/logo.png', canvas_width - 500, 100, width=500, height=500,
@@ -20,15 +17,24 @@ c.drawImage('bin/logo.png', canvas_width - 500, 100, width=500, height=500,
 c.drawString(canvas_width - 500, 12, "Downloaded from https://diplomate.greybits.in/")
 c.save()
 
+# large watermark sample for high res PDFs
+# canvas_width = 566
+# Create the watermark from an image
+# c = canvas.Canvas('bin/sample-watermark--large.pdf')
+# c.drawImage('bin/logo.png', canvas_width+int((canvas_width-1200)/2), 100, width=1200, height=1200,
+#             mask='auto', preserveAspectRatio=True)
+# c.drawString(canvas_width - 500, 12, "Downloaded from https://diplomate.greybits.in/")
+# c.save()
+
 
 right_pdf_list = get_pdf_list()
 
 print("\nRIGHT PDFs : ")
 for booklet_name in right_pdf_list:
     watermark = PdfFileReader(open("bin/sample-watermark.pdf", "rb"))
+    large_watermark = PdfFileReader(open("bin/sample-watermark--large.pdf", "rb"))
     output_file = PdfFileWriter()
     input_file = PdfFileReader(open("input/" + booklet_name, "rb"))
-
     page_count = input_file.getNumPages()
 
     # Go through all the left-watermarker file pages to add a watermark to them
@@ -36,8 +42,16 @@ for booklet_name in right_pdf_list:
         print(booklet_name + ": Watermarking page {} of {}".format(page_number, page_count - 1))
 
         input_page = input_file.getPage(page_number)
-        input_page.mergePage(watermark.getPage(0))
+        # setting a pdf scale to avoid creating different watermarks 
+        input_page.scaleTo(width=612,height=792)
 
+		# changing watermark according to PDF size 
+        # print(list(input_page.mediaBox))
+        # if list(input_page.mediaBox)[3] > 1500:
+        # 	input_page.mergePage(large_watermark.getPage(0))
+        # else:
+        # 	input_page.mergePage(watermark.getPage(0))
+        input_page.mergePage(watermark.getPage(0))
         output_file.addPage(input_page)
 
     with open("output/" + booklet_name, "wb") as outputStream:
